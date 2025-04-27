2f1bf17479a9d44f63bae34c8db0ba67bed5c64d
Andrew Marques <andrewmarques@protonmail.com>
2025-04-27 18:05:00 -0300

    Conclusão
    Conclusão de todas os métodos e menu

--- Aula6Exercicios/aula6exer2/aula6exer2/src/main/java/org/example/Main.java
+++ Aula6Exercicios/aula6exer2/aula6exer2/src/main/java/org/example/Main.java
@@ -31,10 +31,19 @@ public class Main {
             System.out.println("12.  País com maior dimensão do Continente");
             System.out.println("13.  País com menor dimensão do Continente");
             System.out.println("14.  Razão territorial do maior país em relação ao menor país do Continente.");
+            System.out.println("15.  Adicionar automaticamente Continentes e países.");
             System.out.println("0.  Sair do programa.");
-
             opcao = scanner.nextInt();
             scanner.nextLine();
+
+            //variaveis usadas em varios cases
+            String continenteDigitado=null;
+            Continente continenteAlvo = null;
+            Pais paisAlvo = null;
+            boolean continenteEncontrado=false;
+            boolean paisEncontrado=false;
+
+
             switch (opcao) {
                 case 1:
                     System.out.println("1.  Adicionar Continente.");
@@ -95,13 +104,7 @@ public class Main {
                 case 6:
                     System.out.println("6.  Adicionar País ao Continente.");
                     System.out.println("Digite o nome do Continente ao qual deseja adicionar países: ");
-                    String continenteDigitado = scanner.nextLine();
-
-                    Continente continenteAlvo = null;
-                    Pais paisAlvo = null;
-
-                    boolean continenteEncontrado=false;
-                    boolean paisEncontrado=false;
+                    continenteDigitado = scanner.nextLine();
                     for (Continente continente: continentes){
                         if(continente.getNome().equalsIgnoreCase(continenteDigitado)){
                             continenteAlvo = continente;
@@ -126,6 +129,228 @@ public class Main {
                         System.out.println("O continente informado não foi encontrado.");
                     }
                     break;
+                case 7:
+                    System.out.println("7.  Dimensão total do Continente.");
+                    System.out.println("Digite o nome do continente para ver a dimensão total: ");
+                    continenteDigitado = scanner.nextLine();
+                    for (Continente continente: continentes){
+                        if (continente.getNome().equalsIgnoreCase(continenteDigitado)){
+                            continenteAlvo=continente;
+                            continenteEncontrado=true;
+                        }
+                    }
+                    if (continenteEncontrado) {
+                        continenteAlvo.getDimensaoTotalContinente();
+                    } else {
+                        System.out.println("Continente não encontrado.");
+                    }
+                    break;
+                case 8:
+                    System.out.println("8.  População total do Continente.");
+                    System.out.println("Digite o nome do continente para ver a dimensão total: ");
+                    continenteDigitado = scanner.nextLine();
+                    for (Continente continente: continentes){
+                        if (continente.getNome().equalsIgnoreCase(continenteDigitado)){
+                            continenteAlvo=continente;
+                            continenteEncontrado=true;
+                        }
+                    }
+                    if (continenteEncontrado){
+                        continenteAlvo.getPopulacaoTotalContinente();
+                    } else {
+                        System.out.println("Continente não encontrado.");
+                    }
+                    break;
+                case 9:
+                    System.out.println("9.  Densidade populacional do Continente ");
+                    System.out.println("Digite o nome do continente para ver a densidade populacional: ");
+                    continenteDigitado = scanner.nextLine();
+                    for (Continente continente: continentes){
+                        if (continente.getNome().equalsIgnoreCase(continenteDigitado)){
+                            continenteAlvo=continente;
+                            continenteEncontrado=true;
+                        }
+                    }
+                    if (continenteEncontrado){
+                        continenteAlvo.getDensidadePopulacional();
+                    } else {
+                        System.out.println("Continente não encontrado.");
+                    }
+                    break;
+                case 10:
+                    System.out.println("10.  País com maior população do Continente");
+                    System.out.println("Digite o nome do continente para saber o país com maior população: ");
+                    continenteDigitado = scanner.nextLine();
+                    for (Continente continente: continentes){
+                        if (continente.getNome().equalsIgnoreCase(continenteDigitado)){
+                            continenteAlvo=continente;
+                            continenteEncontrado=true;
+                        }
+                    }
+                    if (continenteEncontrado){
+                        continenteAlvo.getPaisMaiorPopulacao();
+                    } else {
+                        System.out.println("Continente não encontrado.");
+                    }
+                    break;
+                case 11:
+                    System.out.println("11.  País com menor população do Continente");
+                    System.out.println("Digite o nome do continente para saber o país com a menor população: ");
+                    continenteDigitado = scanner.nextLine();
+                    for (Continente continente: continentes){
+                        if (continente.getNome().equalsIgnoreCase(continenteDigitado)){
+                            continenteAlvo=continente;
+                            continenteEncontrado=true;
+                        }
+                    }
+                    if (continenteEncontrado){
+                        continenteAlvo.getPaisMenorPopulacao();
+                    } else {
+                        System.out.println("Continente não encontrado.");
+                    }
+                    break;
+                case 12:
+                    System.out.println("12.  País com maior dimensão do Continente");
+                    System.out.println("Digite o nome do continente para saber o país com a maior Dimensão: ");
+                    continenteDigitado = scanner.nextLine();
+                    for (Continente continente: continentes){
+                        if (continente.getNome().equalsIgnoreCase(continenteDigitado)){
+                            continenteAlvo=continente;
+                            continenteEncontrado=true;
+                        }
+                    }
+                    if (continenteEncontrado){
+                        continenteAlvo.getPaisMaiorDimensao();
+                    } else {
+                        System.out.println("Continente não encontrado.");
+                    }
+                    break;
+                case 13:
+                    System.out.println("13.  País com menor dimensão do Continente");
+                    System.out.println("Digite o nome do continente para saber o país com a menor Dimensão: ");
+                    continenteDigitado = scanner.nextLine();
+                    for (Continente continente: continentes){
+                        if (continente.getNome().equalsIgnoreCase(continenteDigitado)){
+                            continenteAlvo=continente;
+                            continenteEncontrado=true;
+                        }
+                    }
+                    if (continenteEncontrado){
+                        continenteAlvo.getPaisMenorDimensao();
+                    } else {
+                        System.out.println("Continente não encontrado.");
+                    }
+                    break;
+                case 14:
+                    System.out.println("14.  Razão territorial do maior país em relação ao menor país do Continente.");
+                    System.out.println("Digite o nome do continente: ");
+                    continenteDigitado = scanner.nextLine();
+                    for (Continente continente: continentes){
+                        if (continente.getNome().equalsIgnoreCase(continenteDigitado)){
+                            continenteAlvo=continente;
+                            continenteEncontrado=true;
+                        }
+                    }
+                    if (continenteEncontrado){
+                        continenteAlvo.getRazao();
+                    } else {
+                        System.out.println("Continente não encontrado.");
+                    }
+                    break;
+                case 15:
+                    continentes.add(new Continente("africa"));
+                    continentes.add(new Continente("antartida"));
+                    continentes.add(new Continente("asia"));
+                    continentes.add(new Continente("europa"));
+                    continentes.add(new Continente("americadonorte"));
+                    continentes.add(new Continente("americadosul"));
+                    continentes.add(new Continente("oceania"));
+
+                    //americadosul
+                    paises.add(new Pais("Brasil", 8510000.0, 216000000));
+                    paises.add(new Pais("Argentina", 2780000.0, 46000000));
+                    paises.add(new Pais("Colombia", 1140000.0, 52000000));
+                    paises.add(new Pais("Peru", 1280000.0, 34000000));
+                    paises.add(new Pais("Chile", 756000.0, 19600000));
+                    //americadonorte
+                    paises.add(new Pais("EstadosUnidos", 9834000.0, 335000000));
+                    paises.add(new Pais("Canada", 9985000.0, 38000000));
+                    paises.add(new Pais("Mexico", 1964000.0, 128000000));
+                    paises.add(new Pais("Cuba", 109800.0, 11000000));
+                    paises.add(new Pais("Guatemala", 108900.0, 18000000));
+                    //europa
+                    paises.add(new Pais("Alemanha", 357000.0, 83000000));
+                    paises.add(new Pais("Franca", 643800.0, 68000000));
+                    paises.add(new Pais("Italia", 301300.0, 59000000));
+                    paises.add(new Pais("Espanha", 505900.0, 47000000));
+                    paises.add(new Pais("Portugal", 92090.0, 10300000));
+                    //asia
+                    paises.add(new Pais("China", 9597000.0, 1400000000));
+                    paises.add(new Pais("Índia", 3287000.0, 1400000000));
+                    paises.add(new Pais("Japao", 377900.0, 125000000));
+                    paises.add(new Pais("CoreiadoSul", 100200.0, 52000000));
+                    paises.add(new Pais("Indonesia", 1905000.0, 278000000));
+                    //africa
+                    paises.add(new Pais("Nigeria", 923800.0, 223000000));
+                    paises.add(new Pais("Egito", 1010000.0, 110000000));
+                    paises.add(new Pais("AfricadoSul", 1221000.0, 60000000));
+                    paises.add(new Pais("Quenia", 580000.0, 55000000));
+                    paises.add(new Pais("Etiopia", 1104000.0, 126000000));
+                    //oceania
+                    paises.add(new Pais("Australia", 7692000.0, 26000000));
+                    paises.add(new Pais("NovaZelandia", 268700.0, 5100000));
+                    paises.add(new Pais("Fiji", 18270.0, 900000));
+                    paises.add(new Pais("PapuaNovaGuine", 462800.0, 10000000));
+                    paises.add(new Pais("Samoa", 2830.0, 200000));
+
+                    for (Continente continente : continentes) {
+                        switch (continente.getNome().toLowerCase()) {
+                            case "americadosul":
+                                continente.addPaises(new Pais("Brasil", 8510000.0, 216000000));
+                                continente.addPaises(new Pais("Argentina", 2780000.0, 46000000));
+                                continente.addPaises(new Pais("Colombia", 1140000.0, 52000000));
+                                continente.addPaises(new Pais("Peru", 1280000.0, 34000000));
+                                continente.addPaises(new Pais("Chile", 756000.0, 19600000));
+                                break;
+                            case "americadonorte":
+                                continente.addPaises(new Pais("EstadosUnidos", 9834000.0, 335000000));
+                                continente.addPaises(new Pais("Canada", 9985000.0, 38000000));
+                                continente.addPaises(new Pais("Mexico", 1964000.0, 128000000));
+                                continente.addPaises(new Pais("Cuba", 109800.0, 11000000));
+                                continente.addPaises(new Pais("Guatemala", 108900.0, 18000000));
+                                break;
+                            case "europa":
+                                continente.addPaises(new Pais("Alemanha", 357000.0, 83000000));
+                                continente.addPaises(new Pais("Franca", 643800.0, 68000000));
+                                continente.addPaises(new Pais("Italia", 301300.0, 59000000));
+                                continente.addPaises(new Pais("Espanha", 505900.0, 47000000));
+                                continente.addPaises(new Pais("Portugal", 92090.0, 10300000));
+                                break;
+                            case "asia":
+                                continente.addPaises(new Pais("China", 9597000.0, 1400000000));
+                                continente.addPaises(new Pais("Índia", 3287000.0, 1400000000));
+                                continente.addPaises(new Pais("Japao", 377900.0, 125000000));
+                                continente.addPaises(new Pais("CoreiadoSul", 100200.0, 52000000));
+                                continente.addPaises(new Pais("Indonesia", 1905000.0, 278000000));
+                                break;
+                            case "africa":
+                                continente.addPaises(new Pais("Nigeria", 923800.0, 223000000));
+                                continente.addPaises(new Pais("Egito", 1010000.0, 110000000));
+                                continente.addPaises(new Pais("AfricadoSul", 1221000.0, 60000000));
+                                continente.addPaises(new Pais("Quenia", 580000.0, 55000000));
+                                continente.addPaises(new Pais("Etiopia", 1104000.0, 126000000));
+                                break;
+                            case "oceania":
+                                continente.addPaises(new Pais("Australia", 7692000.0, 26000000));
+                                continente.addPaises(new Pais("NovaZelandia", 268700.0, 5100000));
+                                continente.addPaises(new Pais("Fiji", 18270.0, 900000));
+                                continente.addPaises(new Pais("PapuaNovaGuine", 462800.0, 10000000));
+                                continente.addPaises(new Pais("Samoa", 2830.0, 200000));
+                                break;
+                        }
+                    }
+
+                    break;
                 case 0:
                     System.out.println("Saindo do programa...");
                     System.out.println();
@@ -137,37 +362,5 @@ public class Main {
             }
         } while (opcao != 0);
         scanner.close();
-
-
-        /*Continente americaDoSul = new Continente("Americano");
-
-        Pais brasil = new Pais("Brasil", 8515770.0, 212600000);
-        Pais argentina = new Pais("Argentina", 2780400.0, 45851378);
-        Pais peru = new Pais("Peru", 1285220.0, 32971846);
-        Pais colombia = new Pais("Colômbia", 1141748.0, 53425635);
-        Pais bolivia = new Pais("Bolívia", 1098581.0, 11673029);
-        Pais venezuela = new Pais("Venezuela", 916447.0, 28435943);
-        Pais chile = new Pais("Chile", 756102.0, 19116209);
-        Pais paraguai = new Pais("Paraguai", 406752.0, 7132530);
-        Pais equador = new Pais("Equador", 283561.0, 17643060);
-        Pais guiana = new Pais("Guiana", 214969.0, 787971);
-        Pais uruguai = new Pais("Uruguai", 176215.0, 3473727);
-        Pais suriname = new Pais("Suriname", 163820.0, 614749);
-
-        americaDoSul.addPaises(brasil);
-        americaDoSul.addPaises(argentina);
-        americaDoSul.addPaises(peru);
-        americaDoSul.addPaises(colombia);
-        americaDoSul.addPaises(bolivia);
-        americaDoSul.addPaises(venezuela);
-        americaDoSul.addPaises(chile);
-        americaDoSul.addPaises(paraguai);
-        americaDoSul.addPaises(equador);
-        americaDoSul.addPaises(guiana);
-        americaDoSul.addPaises(uruguai);
-        americaDoSul.addPaises(suriname);
-        americaDoSul.showPaises();
-        americaDoSul.getDimensaoTotalContinente();
-        americaDoSul.getPopulacaoTotalContinente();*/
     }
 }
--- Aula6Exercicios/aula6exer2/aula6exer2/src/main/java/org/example/modelo/Continente.java
+++ Aula6Exercicios/aula6exer2/aula6exer2/src/main/java/org/example/modelo/Continente.java
@@ -4,11 +4,15 @@ package org.example.modelo;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Continente {
     private String nome;
     private List<Pais> paises = new ArrayList<>();
 
+    DecimalFormat formatoDecimal = new DecimalFormat("#,###");
+    int populacaoTotal = 0;
+    Double dimensaoTotal=0.0;
 
     public Continente(String nome) {
         this.nome = nome;
@@ -31,19 +35,93 @@ public class Continente {
     }
 
     public void getDimensaoTotalContinente(){
-        DecimalFormat formatoDecimal = new DecimalFormat("#,###");
-        Double dimensaoTotal=0.0;
         for (Pais pais: paises){
             dimensaoTotal += pais.getDimensao();
         }
-        System.out.println("A dimensão total do continente " + this.nome + "é aproximadamente: " + formatoDecimal.format(dimensaoTotal) + " km²");
+        System.out.println("A dimensão total do continente " + this.nome + " é aproximadamente: " + formatoDecimal.format(dimensaoTotal) + " km²");
     }
 
     public void getPopulacaoTotalContinente(){
-        int populaçãoTotal = 0;
         for (Pais pais: paises){
-            populaçãoTotal += pais.getPopulacao();
+            populacaoTotal += pais.getPopulacao();
         }
-        System.out.printf("A população total do continente " + this.nome + "é aproximadamente: %,2d", populaçãoTotal);
+        System.out.printf("A população total do continente " + this.nome + " é aproximadamente: %,2d", populacaoTotal);
+    }
+
+    public void getDensidadePopulacional(){
+        for (Pais pais: paises){
+            dimensaoTotal += pais.getDimensao();
+            populacaoTotal += pais.getPopulacao();
+        }
+        Double densidadePopulacionalContinente = this.populacaoTotal/this.dimensaoTotal;
+        System.out.println("A densidade populacional do continente: " + this.nome + " é de: " + formatoDecimal.format(densidadePopulacionalContinente) + "habitantes por km²");
+    }
+
+    public void getPaisMaiorPopulacao(){
+        int maiorPopulacao = 0;
+        String paisMaiorPopulacao = null;
+        for (Pais pais: paises){
+            if (maiorPopulacao < pais.getPopulacao()){
+                maiorPopulacao = pais.getPopulacao();
+                paisMaiorPopulacao = pais.getNome();
+            }
+        }
+        System.out.println("O país com maior população do continente " + this.nome + " é: " + paisMaiorPopulacao);
+    }
+
+    public void getPaisMenorPopulacao(){
+        int menorPopulacao = Integer.MAX_VALUE;
+        String paisMenorPopulacao = null;
+        for (Pais pais: paises){
+            if (menorPopulacao > pais.getPopulacao()){
+                menorPopulacao = pais.getPopulacao();
+                paisMenorPopulacao = pais.getNome();
+            }
+        }
+        System.out.println("O país com menor população do continente " + this.nome + " é: " + paisMenorPopulacao);
+    }
+
+    public void getPaisMaiorDimensao(){
+        double maiorDimensao=0.0;
+        String paisMaiorDimensao = null;
+        for (Pais pais: paises){
+            if (maiorDimensao < pais.getDimensao()){
+                maiorDimensao = pais.getDimensao();
+                paisMaiorDimensao=pais.getNome();
+            }
+        }
+        System.out.println("O país com maior dimensão do continente " + this.nome + " é: " + paisMaiorDimensao);
+    }
+
+    public void getPaisMenorDimensao(){
+        double menorDimensao = Double.MAX_VALUE;
+        String paisMenorDimensao = null;
+        for (Pais pais: paises){
+            if (menorDimensao > pais.getDimensao()){
+                menorDimensao = pais.getDimensao();
+                paisMenorDimensao=pais.getNome();
+            }
+        }
+        System.out.println("O país com menor dimensão do continente " + this.nome + " é: " + paisMenorDimensao);
+    }
+
+    public void getRazao(){
+        double maiorPaisDimensao=0.0;
+        double menorPaisDimensao= Double.MAX_VALUE;
+        double razao =0.0;
+        String maiorNome = null;
+        String menorNome = null;
+        for (Pais pais: paises){
+            if (maiorPaisDimensao < pais.getDimensao()){
+                maiorNome = pais.getNome();
+                maiorPaisDimensao = pais.getDimensao();
+            }
+            if (menorPaisDimensao > pais.getDimensao()){
+                menorNome = pais.getNome();
+                menorPaisDimensao = pais.getDimensao();
+            }
+        }
+        razao = maiorPaisDimensao/menorPaisDimensao;
+        System.out.println("A razão é: " + razao + " entre o pais " + maiorNome + " que é o maior e" + " o país " + menorNome + " que é o menor país.");
     }
 }
