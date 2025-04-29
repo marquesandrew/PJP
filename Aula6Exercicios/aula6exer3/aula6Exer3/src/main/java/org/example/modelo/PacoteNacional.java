1834b87f4b763ea5af4662076fee5be81a1a5f23
Andrew Marques <andrewmarques@protonmail.com>
2025-04-29 15:49:21 -0300

    ajustes

--- Aula6Exercicios/aula6exer3/aula6Exer3/src/main/java/org/example/modelo/PacoteNacional.java
+++ Aula6Exercicios/aula6exer3/aula6Exer3/src/main/java/org/example/modelo/PacoteNacional.java
@@ -1,8 +1,15 @@
 package org.example.modelo;
 
+import java.util.List;
+
 public class PacoteNacional extends PacoteViagem{
     private boolean transporteRodoviario;
 
+    public PacoteNacional(String destino, int duracao, double preco, List<String> atividades) {
+        super(destino, duracao, preco, atividades);
+    }
+
+    @Override
     public String getTipoPacote(){
         return "Nacinoal";
 
--- Aula6Exercicios/aula6exer3/aula6Exer3/src/main/java/org/example/modelo/PacoteViagem.java
+++ Aula6Exercicios/aula6exer3/aula6Exer3/src/main/java/org/example/modelo/PacoteViagem.java
@@ -3,26 +3,24 @@ package org.example.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PacoteViagem {
+public abstract class PacoteViagem {
     private String destino;
-    private String duracao;
+    private int duracao;
     private double preco;
     private List<String> atividades;
 
-    public PacoteViagem(String destino, String duracao, double preco, List<String> atividades) {
+    public PacoteViagem(String destino, int duracao, double preco, List<String> atividades) {
         this.destino = destino;
         this.duracao = duracao;
         this.preco = preco;
-        this.atividades = new ArrayList<>();
+        this.atividades = atividades;
     }
 
-    public void getDetalhesPacoteViagem{
+    public void getDetalhesPacoteViagem(){
         System.out.println("Pacote de Viagem:");
-        System.out.println("Destino: " + this.destino + " - Duração: " + " - Valor: " + this.preco);
+        System.out.println("Destino: " + this.destino + " - Duração: " + this.duracao + " dia(s) - Valor: " + this.preco);
         System.out.println("Atividades durante a viagem: " + this.atividades);
     }
 
-    public String getTipoPacote(){
-
-    }
+    public abstract String getTipoPacote();
 }
